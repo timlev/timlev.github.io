8fc00a932ebf691ac221d5806722f8fabb59a7fb
Tim Leverentz <timlev@gmail.com>
2022-03-12 07:11:21 -0600

    Changed formatting to update_git.sh

--- computer_setup/update_git.sh
+++ computer_setup/update_git.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
 #Define GitProjectsDir
-GitProjectsDir="~/GitProjects/"
+GitProjectsDir="~/GitProjects/";
 
-#Define GitProjectsList
-declare -a StringArray=("Bowls" "Sound-Spelling" "Crowd_Recorder" "Drag_n_Drop" "eartrainer" "Learning-English" "Learning-English-HTML" "Proxy-Hours" "read-story" "sound_board" "timlevhub.io" "download_wiktionary_word" "Pre-Literate-Curriculum" "high-frequency-word-master" "sampleextension")
+#Define GitProjectsList declare -a 
+gplist=("Bowls"
+    "Sound-Spelling"
+    "Crowd_Recorder"
+    "Drag_n_Drop"
+    "eartrainer"
+    "Learning-English"
+    "Learning-English-HTML"
+    "Proxy-Hours"
+    "read-story"
+    "sound_board"
+    "timlevhub.io"
+    "download_wiktionary_word"
+    "Pre-Literate-Curriculum"
+    "high-frequency-word-master"
+    "sampleextension");
 
-for project in ${StringArray[@]}; do
+for project in "${gplist[@]}"; do
     cd $GitProjectsDir;
     cd $project;
     echo "Updating $project"; 
