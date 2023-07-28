7e025ce5ab9929884857fead3150c0fd2ab76afd
Tim Leverentz <timlev@gmail.com>
2023-07-28 07:28:37 -0500

    Added images

--- obituary_os/myScript.js
+++ obituary_os/myScript.js
@@ -9,10 +9,7 @@ What services would you like to avail yourself of?<br>\
 2) Funeral arrangements<br>\
 3) Condolence messages (BETA)<br>\
 4) Other",
-"Excellent! Please enter your loved one's Tax ID Number.",
-"Ed Chapman<br>\
-Roseville, MN<br>\
-Deceased 7/19/59<br>\
+"Excellent! Please enter your loved one's Tax ID Number.", "<img src='Text Box.svg'></img><br>\
 Is that information correct?",
 "Input not valid. Is the above information correct? (Yes or No)",
 "This will take a few moments. Your patience is appreciated.<br>\
@@ -29,15 +26,7 @@ Would you like to use the neuralink to simplify the import process?",
 5) Other (please specify)<br>\
 6) Skip",
 "Profile:<br>\
-30% Sleeping, Limited REM !<br>\
-30% Work<br>\
-25% Food preparation / eating<br>\
-5% Family memories<br>\
-7% Bathroom phone time<br>\
-3% Other<br>\
-<br>\
-Peak emotional engagement:<br>\
-<ul><li>Deadlines,</li><li>bathroom availability,</li><li>uncertainty of what to say,</li><li>birth of child #2</li></ul><br>\
+<img src='Text Box2.png'></img><br>\
 <br>\
 First draft:<br>\
 Bathroom time was very important to Ed. Over 7000 memes were sent. What an accomplishment! Food preparation was also very significant. Favorite recipes included cereal from the box, tangy tuna mac, mac & cheese, and PBJ. His restaurant budget was significant.<br>\
@@ -74,7 +63,8 @@ According to my attention chronology analysis, Ed valued work deadlines, bathroo
 Ed valued getting to church early to get the good seats near the back. Ed had a 49% church attendence record. Ed loved deadlines, bathroom availability, and memes. He had a strong 89% hymn lyric accuracy and only mumbled 10% of the time. During the sermon, he spent 20% of the time thinking about how much the church spent on air conditioning, 30% of the time thinking about the time, and nearly 40% thinking about how his workmates and family members could benefit from hearing this particular part of the sermon. He frequently focused his thoughts on how Child #1 could benefit from portions related to humility, hard work, and &#8230;",
 "Accessing &#8230;<br>\
 Yes, Obituary_1.02 has been a popular choice. Would you like to try that?",
-"Ed Chapman of Roseville, MN died peacefully in his sleep on July 19, 2059. A service will be held at Peace Lutheran Church on Tuesday, July 29, 2059 at 11 AM. Parking is available on site. Ed was preceded in death by his wife Leverne (2053). Ed is survived by his two children, Jean and Martha. Donations can be made to the IBS Foundation.",
+"<img src='Text Box.svg'></img><br>\
+Ed Chapman of Roseville, MN died peacefully in his sleep on July 19, 2059. A service will be held at Peace Lutheran Church on Tuesday, July 29, 2059 at 11 AM. Parking is available on site. Ed was preceded in death by his wife Leverne (2053). Ed is survived by his two children, Jean and Martha. Donations can be made to the IBS Foundation.",
 "Cognitive Processing Fee: $250<br>\
 Computational Time: $35.9999999<br>\
 Religion Import: $85<br>\
@@ -84,7 +74,7 @@ Total: $380.9999999",
 ];
 
 const USR_MSGS = ["two", "One, I guess", "67698","He preferred to go by Edward", "yes","never","skip", "retry", "3", "Retry", "Retry with more focus on relationships", "Let's start with 4, Frank.", "Stop<br>No, I want happy memories. Memories that fit with is values.", "No, I mean values like love, relationships, generosity, and faith", "stop<br>Do you have any generic options?", "for the love of god yes", "Good enough. How much will that cost?", "wow!","No, I think I've had quite enough."]// Icons made by Freepik from www.flaticon.com
-const BOT_IMG = "robot.png";
+const BOT_IMG = "Bot.svg";
 const PERSON_IMG = "https://image.flaticon.com/icons/svg/145/145867.svg";
 const BOT_NAME = "Artificial Assistant";
 var PERSON_NAME = "User_1245";
@@ -165,7 +155,7 @@ document.onload = initialize();
 
 function botResponse() {
   const msgText = BOT_MSGS[bi];
-  const delay = msgText.split(" ").length * 100;
+  const delay = msgText.split(" ").length * 50;
 
   setTimeout(() => {
     appendMessage(BOT_NAME, BOT_IMG, "left", msgText);  bi += 1;
